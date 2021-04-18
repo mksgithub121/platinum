e965341039050153766dca2e3ea4192dc91820a7
Mahadi <mksbd.net@gmail.com>
2021-04-19 04:49:17 +0600

    add

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 $(function () {
 
     //header button
-    $(".header_btn1").on("click", function () {
-        $(".header_btn2").slideToggle();
+    $(".header_section .btn_head").on("click", function () {
+        $(".side_btn_head").slideToggle();
+    });
+    //Search button
+    $(".item_input1").on("click", function () {
+        $(".side_btn_search1").slideToggle();
+    });
+    $(".item_input2").on("click", function () {
+                $(".side_btn_search2").slideToggle();
     });
 
 
