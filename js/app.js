b2ad4010d6c5f2ec6389767ef79f516d02abeae8
Mahadi <mksbd.net@gmail.com>
2021-04-18 17:13:47 +0600

    header add

---
+++ js/app.js
@@ -0,0 +1,74 @@
+$(function () {
+
+    //header button
+    $(".header_btn1").on("click", function () {
+        $(".header_btn2").slideToggle();
+    });
+
+
+
+
+
+    // preloader code  here
+    $(window).on("load", function () {
+        $(".preloader").addClass("complete");
+    });
+
+    //    scroll animation aos plugin code  here 
+    AOS.init();
+
+    //  Sticky menu js code  here 
+    //  scroll down to hide navbar and scroll top to show navbar
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > 50 && prevScrollpos > currentScrollPos) {
+            $(".header_section").addClass("sticky");
+        } else {
+            $(".header_section").removeClass("sticky");
+        }
+        prevScrollpos = currentScrollPos;
+    }
+
+
+    //  scroll down to show navbar 
+
+    $(window).on("scroll", function () {
+        var scrollAmount = $(window).scrollTop();
+        if (scrollAmount > 200) {
+            $(".header_section").addClass("sticky");
+        } else {
+            $(".header_section").removeClass("sticky");
+        }
+    });
+
+    // Back To Top button  here
+    $(window).on("scroll", function () {
+        var scrollAmount = $(window).scrollTop();
+        if (scrollAmount > 200) {
+            $(".backToTop").slideDown();
+
+        } else {
+            $(".backToTop").slideUp();
+        }
+    });
+    $(".backToTop i").on("click", function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 1500);
+    });
+    //  Smooth section scroll code  here 
+    $(".header_section .menu ul li a").on('click', function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function () {
+            // window.location.hash = target;
+        });
+    });
+
+
+});
